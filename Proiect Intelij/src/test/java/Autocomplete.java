b9831fcb1f36e6f6a748e0d97482122604d7dd1e
danyel82 <99751779+danyel82@users.noreply.github.com>
2022-02-15 21:09:39 +0200

    Creare proiect

---
+++ Proiect Intelij/src/test/java/Autocomplete.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","D:/Curs SDA/chromedriver_win32" );
+        // la linia 6 am spus sistemului unde sa gaseasca driverul
+        WebDriver driver = new ChromeDriver();
+        //Aici am creat un nou obiect al clasei chrome driver care se numeste driver si care are tipul de date webdriver.
+
+    /* -O variabila este o adresa de memorie la care sunt stocate informatii
+       -O functie reprezinta un set de instructiuni grupate sub un singur nume
+       -O clasa reprezinta un tipar care defineste atributele pe care le are un obiect si actiunulie pe care trebuie sa le faca
+       - un obiect este o instanta a unei clase adica o reprezentare reala creata pe baza tiparului
+       - un tip de data este o proprietate a unei variabile
+     */
+    }
+}
